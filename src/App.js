31fb04303e43de9380f83a6e395c170837373c14
MaryPirtu <maria.pirtu@hotmail.com>
2023-03-17 15:41:11 +0000

    Change date look

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
@@ -8,11 +10,11 @@ function App() {
       amount: 94.12,
       date: new Date(2020, 7, 14),
     },
-    { 
-      id: "e2", 
-      title: "New TV", 
-      amount: 799.49, 
-      date: new Date(2021, 2, 12) 
+    {
+      id: "e2",
+      title: "New TV",
+      amount: 799.49,
+      date: new Date(2021, 2, 12),
     },
     {
       id: "e3",
@@ -23,7 +25,7 @@ function App() {
     {
       id: "e4",
       title: "New Desk (Wooden)",
-      amount: 450,
+      amount: 450.35,
       date: new Date(2021, 5, 12),
     },
   ];
